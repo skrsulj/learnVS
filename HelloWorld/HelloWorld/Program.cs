894ea9dab26ee062e0637ef43e262485e6778687
Sasa Krsulj <sasa.krsulj@gmail.com>
2018-01-23 12:37:26 -0500

    changed line 2

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -11,7 +11,7 @@ namespace HelloWorld
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Added line 2!");
+            Console.WriteLine("Changed line 2!");
             Console.WriteLine("Line 3 Jenkins WORKS!!!");
         }
     }
